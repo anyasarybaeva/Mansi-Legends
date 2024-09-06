9d97f2930e62cdc2cf8bf339293f2048535dfaf1
Oksana Solomenchuk <o.solomenchuk@mtsai.team>
2024-09-06 19:31:39 +0300

    sorted dataset

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -5,10 +5,21 @@ import pandas as pd
 path = os.getcwd() + '/preprocess/'
 data = pd.read_csv(path + "overall_80K.csv")
 
-mydata = data["source"]
-# mydata = ["Пēс наканув э̄лаль вос о̄лэ̄гыт"]
+count = -1
+for example in data["source"]:
+    count += 1
+    # убираем потенциально мансийские названия в кавычках
+    cut_version = re.sub('[\"«].*?[»\"]', '', example)
+    # убираем потенциально мансийские названия населенных пунктов (но тут может уже дать осечку)
+    cut_version = re.sub('([А-Я]|А̄|Ē|Ё̄|О̄|Ы̄|Ю̄|Э̄|Я̄|Ӯ|Ӈ)([а-я]|а̄|ē|ё̄|о̄|ы̄|ю̄|э̄|я̄|ӯ|ӈ)+(-([А-Я]|А̄|Ē|Ё̄|О̄|Ы̄|Ю̄|Э̄|Я̄|Ӯ|Ӈ|[а-я]|а̄|ē|ё̄|о̄|ы̄|ю̄|э̄|я̄|ӯ|ӈ)+)?', '', cut_version)
+    if len(re.findall('(а̄|ē|ё̄|о̄|ы̄|ю̄|э̄|я̄|ӯ|ӈ|А̄|Ē|Ё̄|О̄|Ы̄|Ю̄|Э̄|Я̄|Ӯ|Ӈ)', cut_version)) > 0:
+        # один несчастый пример с дурацкими символами
+        if example.startswith("Иван Егорович был из деревни") == True:
+            continue
+        new_source = data["target"][count]
+        new_target = data["source"][count]
+        data.at[count, "target"] = new_target
+        data.at[count, "source"] = new_source
 
-for example in mydata:
-    # print(example)
-    if re.search('[а̄ēё̄оы̄ю̄э̄я̄А̄ĒЁ̄О̄Ы̄Ю̄Э̄Я̄̄ӮӯӇӈ]', example, re.UNICODE):
-        print(example)
+data.to_csv(path + 'overall_80K_sorted.csv', sep=',', encoding='utf-8', index=False)
+print("all done!")
