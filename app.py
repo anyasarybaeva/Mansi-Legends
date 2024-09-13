8b34949494b14b0dfe51cbdfbb769a25d57f8cfe
HNBA103042 <hnba103042@HNBA.local>
2024-09-13 19:52:24 +0300

    add web

---
+++ app.py
@@ -0,0 +1,53 @@
+from inference import load, translate
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
+
+# Загрузка модели и токенизатора
+model_path = "600m_added_vocab"  # Укажите путь к вашей модели
+model, tokenizer = None, None
+app = FastAPI()
+
+# Разрешаем CORS для взаимодействия с фронтендом
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Позволяет всем источникам доступ к API, можно сузить до нужных доменов
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class TextInput(BaseModel):
+    text: str
+
+@app.post("/process")
+async def process_text(input_data: TextInput):
+    input_text = input_data.text
+    print("Received call", input_text)
+    # Пример обработки текста моделью
+    output_text = translate(input_text, model, tokenizer)
+
+    return {"output": output_text}
+
+# Инициализация FastAPI
+
+@app.on_event("startup")
+async def startup_event():
+    global model, tokenizer
+    model, tokenizer = load(model_path)
+
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("htmlMain.html", {"request": request})
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host='127.0.0.1', port=8000)
